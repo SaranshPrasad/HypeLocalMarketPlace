add8e9b343f7f71b2a76c0c305280a052ac55256
Saransh Bari <131943503+SaranshPrasad@users.noreply.github.com>
2026-04-19 19:43:58 +0530

    Update MongoDB connection string to remove password
    Removed password from MongoDB connection string.

--- Database/connection.js
+++ Database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const db = await mongoose.connect(
-      `mongodb+srv://saranshprasad08:LZ36unwnOiYttSRY@alumini.hyktp.mongodb.net/?appName=Alumini`,
+      `mongodb+srv://saranshprasad08:@alumini.hyktp.mongodb.net/?appName=Alumini`,
     );
     console.log("Database connected!");
   } catch (error) {
